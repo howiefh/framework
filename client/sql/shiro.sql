8ca27eed05bf2bec4b5b9ecc1025e4d8883c4a7a
Feng Hao <howiefh@gmail.com>
2015-05-15 20:08:28 +0800

    增强对用户凭证的认证方式，可以配置hash算法，迭代次数，盐，sql语句，查询字段名称。 散列函数库使用shiro

--- client/sql/shiro.sql
+++ client/sql/shiro.sql
@@ -45,4 +45,7 @@ create table sys_roles_permissions (
 ) charset=utf8 ENGINE=InnoDB;
 
 -- md5("password") 5f4dcc3b5aa765d61d8327deb882cf99
-insert into sys_users(username,password) values("test","5f4dcc3b5aa765d61d8327deb882cf99");
+INSERT INTO `sys_users` (`id`, `username`, `password`, `salt`, `locked`) VALUES ('2', 't1', '5f4dcc3b5aa765d61d8327deb882cf99', '', '0');
+-- SimpleHash("md5","123456","8d78869f470951332959580424d4bf4f",2) dd3736d1c3c4702e5170cde08bffd411
+INSERT INTO `sys_users` (`id`, `username`, `password`, `salt`, `locked`) VALUES ('3', 't2', 'dd3736d1c3c4702e5170cde08bffd411', '8d78869f470951332959580424d4bf4f', '0');
+
